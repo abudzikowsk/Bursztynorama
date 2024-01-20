52aa453e237607fb200cda53eec8c7d68e425b42
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2024-01-20 21:26:30 +0100

    add migrations

---
+++ Bursztynorama/Migrations/20240120194343_initial migration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bursztynorama.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeatherHistoricalData",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Cities = table.Column<int>(type: "INTEGER", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    AirTemperature = table.Column<double>(type: "REAL", nullable: false),
+                    WindSpeed = table.Column<double>(type: "REAL", nullable: false),
+                    WindDirection = table.Column<string>(type: "TEXT", nullable: false),
+                    MoonPhase = table.Column<int>(type: "INTEGER", nullable: true),
+                    SeaTemperature = table.Column<double>(type: "REAL", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeatherHistoricalData", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeatherHistoricalData");
+        }
+    }
+}
---
+++ Bursztynorama/Migrations/20240120200822_fix city name in entity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bursztynorama.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixcitynameinentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cities",
+                table: "WeatherHistoricalData",
+                newName: "City");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "City",
+                table: "WeatherHistoricalData",
+                newName: "Cities");
+        }
+    }
+}
