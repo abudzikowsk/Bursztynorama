57b0e28aa1a8c99f715ec503d8d36bbb5312c538
Aleksandra Budzikowska <107808781+abudzikowsk@users.noreply.github.com>
2024-03-05 20:40:47 +0100

    fix mobile view and add autohide popup

--- Bursztynorama/ClientApp/src/app/popup/popup.component.ts
+++ Bursztynorama/ClientApp/src/app/popup/popup.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {Component, ElementRef, HostListener, Input, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-popup',
@@ -10,9 +10,21 @@ import {Component, ElementRef, Input, ViewChild} from '@angular/core';
 export class PopupComponent {
   @Input() public text = '';
   @ViewChild('popup', { static: false }) popup!: ElementRef;
+  @ViewChild('icon', { static: false }) icon!: ElementRef;
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    if (!this.icon.nativeElement.contains(event.target)) {
+      this.hidePopup();
+    }
+  }
 
   showPopup() {
     this.popup.nativeElement.classList.toggle("show");
+  }
+
+  hidePopup() {
+    this.popup.nativeElement.classList.remove("show");
   }
 }
 
